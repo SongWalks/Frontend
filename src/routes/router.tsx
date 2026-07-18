d98d2f7ca6389d3808d6b5aa9d8fd7ea6dd2f3c1
yoooooojin <ujin4903@sookmyung.ac.kr>
2026-07-18 18:06:27 +0900

    chore: test 파일들 모두 제거

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -5,16 +5,7 @@ import RootLayout from '@/components/layout/RootLayout';
 import DefaultLayout from '@/components/layout/DefaultLayout';
 import FullScreenLayout from '@/components/layout/FullScreenLayout';
 
-// --- 2. 페이지 불러오기 (임시 예시) ---
-import { TestButton } from '../pages/TestButton';
-import { TestHeader } from '../pages/TestHeader';
-import { TestInput } from '../pages/TestInput';
-import { TestModal } from '../pages/TestModal';
-import { TestTabs } from '../pages/TestTabs';
-import { TestCard } from '../pages/TestCard';
-import { TestExtra } from '../pages/TestExtra';
-import { TestAvatar } from '../pages/TestAvatar';
-import { TestLoading } from '../pages/TestLoading';
+// --- 2. 페이지 불러오기  ---
 
 export const router = createBrowserRouter([
   {
@@ -26,16 +17,7 @@ export const router = createBrowserRouter([
         // ==========================================
         element: <DefaultLayout />,
         children: [
-          // 예시: { path: 'board', element: <BoardPage /> },    // /board (교환게시판)
-          { path: '/test-input', element: <TestInput /> },
-          { path: '/test-modal', element: <TestModal /> },
-          { path: '/test-button', element: <TestButton /> },
-          { path: '/test-header', element: <TestHeader /> },
-          { path: '/test-tabs', element: <TestTabs /> },
-          { path: '/test-card', element: <TestCard /> },
-          { path: '/test-extra', element: <TestExtra /> },
-          { path: '/test-avatar', element: <TestAvatar /> },
-          { path: '/test-loading', element: <TestLoading /> },
+          // 예시: { path: '/board', element: <BoardPage /> },    // /board (교환게시판)
         ],
       },
       {
@@ -45,7 +27,7 @@ export const router = createBrowserRouter([
         element: <FullScreenLayout />,
         children: [
           // :id 나 :roomId 는 동적 라우팅 기법입니다. (ex. /board/123)
-          // 예시: { path: 'board/:id', element: <DetailPage /> },    // 상세 게시글
+          // 예시: { path: '/board/:id', element: <DetailPage /> },    // 상세 게시글
         ],
       },
     ],
